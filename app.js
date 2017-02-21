4626649534c44f31d387cab7767b79369947f6c0
wang jie <wangjie@wangmatoAir.lan>
2017-02-21 22:27:37 +0800

    watch http request

--- app.js
+++ app.js
@@ -8,17 +8,19 @@ let fs      = require('fs');
 let mime    = require('mime');
 let port    = 8000;
 
-function comboRequestList(req){
+function comboRequestList(req,res){
 
     let parseUrl = url.parse(req.url);
-    console.log(req,parseUrl);
 
     let list = {
-        name: parseUrl.pathname,
-        status: 200,
+        name: parseUrl.pathname == "/" ? parseUrl.href : parseUrl.pathname.split('/').pop(),
+        statusCode: 'pendding',
+        statusMessage: '',
+        url:parseUrl.href,
         type: mime.lookup(req.url),
         size: 0
     };
+    console.log(list);
     return list;
 }
 
@@ -26,34 +28,57 @@ function watchProxy(self){
 
     let app = connect();
 
-    app.use(function(req,AppRes){
+    app.use(function(req,res){
 
+        let list = comboRequestList(req,res);
 
-        self.list.push(comboRequestList(req));
+        self.list.push(list);
 
         let options = {
             url: req.url,
             method: req.method,
-            encoding:'utf-8',
             headers: req.headers
         };
 
-        if(options.url.lastIndexOf('.css') !== -1){
-            let filePath = '*.css';
-            fs.stat(filePath, function(err, stat){
-                if(err){ throw err; }
-                if(!stat.isFile()){
-                  throw new Error('The responder is not a file!');
-                }
+        var body = '';
+        res.on('data', function(chunk) {
+            body += chunk;
+        });
+        res.on('end', function() {
+            console.log(res);
+        });
 
-                AppRes.statusCode = 200;
-                AppRes.setHeader('Content-Length', stat.size);
-                AppRes.setHeader('Content-Type', mime.lookup(filePath));
-                AppRes.setHeader('Server', 'nproxy');
-                fs.createReadStream(filePath).pipe(AppRes);
-            });
-        }else{
-            request.get(options.url).pipe(AppRes);
+        console.log(req);
+        // console.log(res);
+
+        request(options).on('response', function(response) {
+
+            console.log(response);
+
+            // let index = self.list.indexOf(list);
+            // self.list[index].status = response.statusCode;
+            list.statusCode = response.statusCode;
+            list.statusMessage = response.statusMessage;
+            list.size = (response.headers['content-length']/1000).toFixed(1) + ' KB';
+            
+        }).pipe(res);
+
+        // if(options.url.lastIndexOf('.css') !== -1){
+        //     let filePath = '*.css';
+        //     fs.stat(filePath, function(err, stat){
+        //         if(err){ throw err; }
+        //         if(!stat.isFile()){
+        //           throw new Error('The responder is not a file!');
+        //         }
+
+        //         AppRes.statusCode = 200;
+        //         AppRes.setHeader('Content-Length', stat.size);
+        //         AppRes.setHeader('Content-Type', mime.lookup(filePath));
+        //         AppRes.setHeader('Server', 'nproxy');
+        //         fs.createReadStream(filePath).pipe(AppRes);
+        //     });
+        // }else{
+        
             // request.get(options.url,function(error,res,body){
             //     if(error) { console.error(error);}
             //     if(res.headers['content-type'] == 'image/jpeg'){
@@ -64,13 +89,12 @@ function watchProxy(self){
             //         AppRes.end();
             //     }
             // });
-        }
+        // }
 
     });
 
 
     let server = http.createServer(function(req, res) {
-        req.type = 'http';
         app(req, res);
     }).listen(port);
 
